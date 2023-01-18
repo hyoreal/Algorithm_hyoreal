6780824f19e85511943fbb249532f735cb2c0c10
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-01-18 22:04:41 +0900

    [Silver V] Title: 덩치, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] person = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            person[i][0] = Integer.parseInt(st.nextToken());
+            person[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            int rank = 1;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (person[i][0] < person[j][0] && person[i][1] < person[j][1]) ++rank;
+            }
+            sb.append(rank).append(" ");
+        }
+        System.out.print(sb);
+    }
+}
