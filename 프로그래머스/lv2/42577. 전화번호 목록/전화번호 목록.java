316aace57d4e1903336a41facaed2fe450cf39e8
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-08-03 21:45:10 +0900

    [level 2] Title: 전화번호 목록, Time: 199.25 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Set<String> set = new HashSet<>();
+        for (String p : phone_book) set.add(p);
+        
+        for (String p : phone_book) {
+            for (int i = 1; i < p.length(); i++) {
+                if (set.contains(p.substring(0, i))) return false;
+            }
+        }
+        return true;
+    }
+}
