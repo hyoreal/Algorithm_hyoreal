066c4e45f066e8f4a98829871799bba23c3b737d
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-08-03 21:30:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.92 ms, Memory: 95.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String ans = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String p : participant) map.put(p, map.getOrDefault(p, 0) + 1);
+        for (String c : completion) map.put(c, map.get(c) - 1);
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<String, Integer> entry = iter.next();
+            if (entry.getValue() != 0) {
+                ans = entry.getKey();
+                break;
+            }
+        }
+        return ans;
+    }
+}
