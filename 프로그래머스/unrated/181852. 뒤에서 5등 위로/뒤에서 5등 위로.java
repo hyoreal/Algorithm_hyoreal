6d410a30bb97f6f981a7472c9547a83cfc27584c
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-09-04 16:12:39 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 6.43 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        return Arrays.copyOfRange(num_list, 5, num_list.length);
+        return Arrays.stream(num_list).sorted().skip(5).toArray();
     }
 }
