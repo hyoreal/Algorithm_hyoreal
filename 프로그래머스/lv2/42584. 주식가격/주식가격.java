95e93edc6406d8c7183db1f3bd2a8d158e8c5827
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-09-06 23:44:20 +0900

    [level 2] Title: 주식가격, Time: 23.22 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -8,12 +8,12 @@ class Solution {
         for (int i = 0; i < prices.length; i++) {
             while (!q.isEmpty() && prices[i] < prices[q.peek()]) {
                 answer[q.peek()] = i - q.peek();
-                q.pop();  // 답을 구했기 때문에 stack에서 제거
+                q.pop();
             }
             q.push(i);
         }
 
-        while (!q.isEmpty()) { // 값을 구하지 못한 index == 끝까지 가격이 떨어지지 않은 주식
+        while (!q.isEmpty()) {
             answer[q.peek()] = prices.length - q.peek() - 1;
             q.pop();
         }
