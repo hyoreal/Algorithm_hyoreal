cbb6489b0aa0a80e2b65a5195b033d2c7b8b01e2
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-09-06 23:04:23 +0900

    [level 2] Title: 프로세스, Time: 0.55 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
+
+        for (int priority : priorities) pq.add(priority);
+
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) { 
+                    if (i == location) {
+                        answer++;
+                        return answer;
+                    }   
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+        return -1;
+    }
+}
