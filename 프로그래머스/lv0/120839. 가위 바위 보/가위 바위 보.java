02e44bdfd919b10bc80a40e8e5324eabaee6950a
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-09-11 23:37:26 +0900

    [level 0] Title: 가위 바위 보, Time: 2.78 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String rsp) {
+        return Arrays.stream(rsp.split("")).map(s -> s.equals("2") ? "0" : s.equals("0") ? "5" : "2").collect(Collectors.joining());
+    }
+}
