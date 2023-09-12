77cf2688ee82f8748739986609d2e25787ad352f
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-09-12 18:50:12 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 3.55 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,7 +1,7 @@
-import java.util.*;
+import java.util.stream.Stream;
 
 class Solution {
     public int[] solution(String my_string) {
-        return Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
+        return Stream.of(my_string.replaceAll("[^0-9]", "").split("")).mapToInt(Integer::parseInt).sorted().toArray();
     }
 }
