eb2e84d6d8a97200e4bdb2458cde74a6af8b4c33
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-09-16 11:37:40 +0900

    [unrated] Title: 글자 지우기, Time: 1.49 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String[] arr = my_string.split("");
+
+        for (int i = 0; i < indices.length; i++) arr[indices[i]] = "";
+
+        return Arrays.stream(arr).collect(Collectors.joining());
+    }
+}
