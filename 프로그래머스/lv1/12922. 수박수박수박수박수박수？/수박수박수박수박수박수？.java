610693afe7836507aadef1baec7cc7190840eafd
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-09-19 21:52:45 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.26 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,8 @@
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(int n) {
+        return IntStream.rangeClosed(1, n).mapToObj(i -> i % 2 == 1 ? "수" : "박").collect(Collectors.joining());
+    }
+}
