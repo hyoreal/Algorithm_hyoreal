20a0d223708c38692aaf2f18402f5aee8879d08a
hyoreal <102732425+hyoreal@users.noreply.github.com>
2023-09-20 18:38:33 +0900

    [level 0] Title: 문자열 계산하기, Time: 1.78 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String my_string) {
+        return Arrays.stream(my_string.replaceAll("- ", "-").replaceAll("[+] ", "").trim().split(" ")).mapToInt(Integer::parseInt).sum();
+    }
+}
